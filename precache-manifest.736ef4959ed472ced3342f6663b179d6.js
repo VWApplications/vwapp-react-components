7b99ace56b0a92de3f2b3a8a280e1020c7d112aa
Victor Arnaud <victorhad@gmail.com>
2020-04-13 05:28:10 -0300

    Updates

---
+++ precache-manifest.736ef4959ed472ced3342f6663b179d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "427fa5b19a732a4f86829c379663e109",
+    "url": "/vwapp-react-components/index.html"
+  },
+  {
+    "revision": "6d55a5d3605aac2b8663",
+    "url": "/vwapp-react-components/static/css/2.5f71d73f.chunk.css"
+  },
+  {
+    "revision": "6d55a5d3605aac2b8663",
+    "url": "/vwapp-react-components/static/js/2.77e2d5e8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/vwapp-react-components/static/js/2.77e2d5e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a64e5e2b6dde00e82ab8",
+    "url": "/vwapp-react-components/static/js/main.4c039eb6.chunk.js"
+  },
+  {
+    "revision": "3630eb673c14e5a4839e",
+    "url": "/vwapp-react-components/static/js/runtime-main.14bb5b4b.js"
+  },
+  {
+    "revision": "6102afa3a36a1b04edb04d17e64a3f1e",
+    "url": "/vwapp-react-components/static/media/user~bWEoNccF.6102afa3.png"
+  }
+]);
