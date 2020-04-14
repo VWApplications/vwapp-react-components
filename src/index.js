66535052b80c402251adc36f20874a68f7f60da5
Victor Arnaud <victorhad@gmail.com>
2020-04-14 03:52:07 -0300

    Remove flex constants

--- src/index.js
+++ src/index.js
@@ -4,8 +4,5 @@ export { Pagination } from './components/Pagination';
 export { FlexContainer, FlexItem } from './components/Flexbox';
 // Outros
 export { Line, BreakLine, Json } from './components/Generics';
-export {
-  Flexbox, Colors, Typography, Border, Measures, Spacing, Shadow,
-  Visibility, Position, Display, Others
-} from './constants';
+export { Colors, Typography, Border, Measures, Spacing, Shadow, Visibility, Position, Display, Others } from './constants';
 export { toString, isEmpty, formatWithLeftZero, moveToTop } from './utils';
