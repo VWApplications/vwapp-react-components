2cace8c922d38d79ad7d2e2ac0575c8d919b00af
Victor Arnaud <victorhad@gmail.com>
2020-04-16 21:34:36 -0300

    Insert custom scroll

---
+++ example/src/components/CustomScroll.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import { CustomScroll } from 'vwapp-react-components';
+
+class MyCustomScroll extends Component {
+  render() {
+    return (
+      <div style={{height: '300px'}}>
+        <CustomScroll className="pr-3">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut consequat magna. Vivamus id odio
+            vel urna convallis feugiat. Phasellus sit amet odio turpis. Sed vehicula urna sit amet metus accumsan
+            blandit. Donec mollis erat elit, at accumsan sem dapibus quis. Sed eu rutrum augue, ac imperdiet arcu.
+            Aliquam nec dictum mi. Curabitur aliquam ornare est, et mollis nulla malesuada at.
+          </p>
+
+          <p>
+            Sed imperdiet enim at augue maximus pulvinar. Sed volutpat risus nisi, finibus lacinia mauris sollicitudin ac.
+            Suspendisse lorem erat, finibus ac leo et, viverra rutrum nisi. Donec cursus nulla orci, a condimentum ex
+            convallis ut. Quisque quis libero tincidunt, tempus metus a, venenatis metus. Aenean in mauris dolor. Aliquam
+            erat volutpat. Donec semper fermentum arcu. Vivamus ante tellus, ullamcorper et congue id, pellentesque non leo.
+            Maecenas suscipit fermentum ultrices. Etiam blandit, sapien ac euismod scelerisque, nunc felis tristique urna,
+            sit amet malesuada nisi velit ut felis. Maecenas sit amet laoreet sapien. Nunc quis risus tellus. Vestibulum ante
+            ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;
+          </p>
+
+          <p>
+            Praesent sollicitudin, libero ut rhoncus finibus, massa nulla rutrum nisl, ac euismod nisi dolor ac justo.
+            Integer in dictum tellus, quis consectetur velit. Suspendisse enim mauris, ultrices in luctus et, luctus rhoncus
+            odio. Maecenas fringilla eleifend lectus, in ornare arcu pellentesque sed. Praesent ullamcorper sollicitudin tempor.
+            Fusce condimentum eu velit at placerat. Vestibulum pharetra dui auctor felis cursus consectetur. Nullam risus ex,
+            ultrices vitae consequat sed, bibendum eget eros. Donec ultrices lectus vitae tortor tincidunt ullamcorper. Praesent
+            non enim eu elit commodo bibendum. Etiam eu varius neque. Sed pulvinar, felis eget interdum mattis, ex ante sodales
+            lectus, vel convallis metus felis a augue. Etiam placerat placerat interdum. Vestibulum condimentum pharetra sem sit
+            amet tempor. Vivamus varius ultrices quam, eget bibendum turpis tincidunt eget.
+          </p>
+
+          <p>
+            In porttitor nisi nisi. Vivamus tincidunt ipsum sed aliquam feugiat. Vivamus a sapien sagittis, tempor felis ac,
+            ultrices lectus. Aliquam vestibulum felis ut convallis eleifend. Donec nec efficitur ipsum. Sed id mi ac nibh hendrerit
+            tincidunt id at dolor. Integer interdum, nibh a ultricies elementum, mauris sem faucibus lectus, quis ultrices lorem
+            risus sed nunc. Curabitur sodales nisl quis porta suscipit. Donec cursus nisl et semper varius. Duis tempus laoreet vulputate.
+          </p>
+
+          <p>
+            Pellentesque dolor erat, sagittis eget est eu, tempus mollis purus. Donec vestibulum tempor quam eget lobortis.
+            Aenean consequat dignissim dolor. Integer non maximus ante. Morbi vitae urna non orci hendrerit semper ac at ligula.
+            Sed hendrerit feugiat nisi, a aliquet lorem semper vel. In sed enim vehicula, mattis sapien ac, tempus turpis. Maecenas
+            viverra semper posuere. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec pretium, sem eu consequat
+            pharetra, ligula neque pharetra orci, eu dignissim neque dui at turpis. Quisque ac eros non nisi cursus lacinia et ac odio.
+            Duis lectus metus, porta nec arcu in, fringilla ornare quam. Duis cursus pretium tortor ut elementum. Sed bibendum
+            vestibulum lectus, et finibus mauris sodales sit amet.
+          </p>
+        </CustomScroll>
+      </div>
+    )
+  }
+}
+
+export default MyCustomScroll;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import CustomScroll from 'react-customscroll';
 // Componentes
 export { Pagination } from './components/Pagination';
 export { FlexContainer, FlexItem } from './components/Flexbox';
 export { Line, BreakLine, Json } from './components/Generics';
 export { Utilities } from './utilities';
+export { CustomScroll };
